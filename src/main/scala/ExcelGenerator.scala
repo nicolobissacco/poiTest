c6a21a2075fa9538e944e856372e0b1386faa912
Nicolò Bissacco <nicolo.bissacco@warda.it>
2019-11-29 15:25:36 +0100

    ...

--- src/main/scala/ExcelGenerator.scala
+++ src/main/scala/ExcelGenerator.scala
@@ -4,10 +4,11 @@ import java.time.format.DateTimeFormatter
 
 import org.apache.poi.ss.usermodel.{Sheet, Workbook}
 import org.apache.poi.xssf.streaming.SXSSFWorkbook
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
 object ExcelGenerator extends App {
   val headers: Seq[(String, String)] = for (i <- 0 until 70) yield (s"ID-${i}", s"DESC-${i}")
-  val products: Seq[Map[String, String]] = for (i <- 0 until 50000) yield (for (c <- 0 until headers.length) yield headers(c)._1 -> s"desc-P${i}-${c}").toMap
+  val products: Seq[Map[String, String]] = for (i <- 0 until 100000) yield (for (c <- 0 until headers.length) yield headers(c)._1 -> s"desc-P${i}-${c}").toMap
 
   generateXlsx("file.xlsx", headers, products)
 
@@ -16,11 +17,13 @@ object ExcelGenerator extends App {
     println(s"START ${LocalDateTime.now().format(fmt)}")
 
     val wb = new SXSSFWorkbook(SXSSFWorkbook.DEFAULT_WINDOW_SIZE)
+    //val wb = new XSSFWorkbook
     val sheet1 = wb.createSheet("Foglio1")
 
     createHeader(wb, sheet1, headers.map(x => x._2))
     createRows(wb, sheet1, headers.map(x => x._1), items)
     saveToFile(wb, filePath);
+    wb.dispose
 
     println(s"END ${LocalDateTime.now().format(fmt)}")
   }
@@ -28,11 +31,11 @@ object ExcelGenerator extends App {
   def createRows = (wb: Workbook, sheet: Sheet, headers: Seq[String], items: Seq[Map[String, String]]) => {
     for (r <- 0 until items.length; c <- 0 until headers.length) {
       val row = {
-        if (sheet.getRow(r + 11) == null) {
+        if (sheet.getRow(r + 1) == null) {
           sheet.createRow(r + 1)
         }
         else {
-          sheet.getRow(r + 11)
+          sheet.getRow(r + 1)
         }
       }
       row.createCell(c).setCellValue(items(r).getOrElse(headers(c), ""))
